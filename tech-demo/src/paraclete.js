6957243cee305a22dc2568285ac8595c6a1b0254
paraclete12 <oliverparaclete@gmail.com>
2026-02-01 18:48:41 +0100

    paraclete colloraboration updated code

--- tech-demo/src/paraclete.js
+++ tech-demo/src/paraclete.js
@@ -1,1 +1,9 @@
 // this is just a coloraboration
+test('postdepartments', async ({ request }) => {
+  const response = await request.post ('http://localhost:5000/departments',{
+    headers:{'content-Type':'application/json'},
+    data:{"name":"Product Design"}
+  })
+  const obj = await response.json()
+  console.log(obj)
+})
