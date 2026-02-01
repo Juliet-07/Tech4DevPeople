41f092376788d4da41ab753a3636513e730434d5
Ruth-techbabe <oziomamichelle33@gmail.com>
2026-02-01 19:24:17 +0100

    update on findAllUsers

---
+++ tech-demo/src/ruth.js
@@ -0,0 +1,9 @@
+const findAllUsers = async (req, res) => {
+ try {
+ const users = await User.findAll()
+ res.status(200).json({users})
+ } catch (error) {
+ console.log(error)
+ res.status(500).json({error: 'Failed to retrieve users'})
+ }
+}
